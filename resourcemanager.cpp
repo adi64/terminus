d65322c9c826e0f199729506fed695b0a5e89d69
Lukas Durdel <ldurdel@sys108>
2014-11-26 10:11:30 +0100

    more code for resourcemanager

--- resourcemanager.cpp
+++ resourcemanager.cpp
@@ -2,14 +2,6 @@
 
 #include "geometry.h"
 
-//initialize singleton
-ResourceManager::m_instance();
-
-ResourceManager::ResourceManager & get()
-{
-    return & m_instance;
-}
-
 ResourceManager::ResourceManager()
 {
 
@@ -17,15 +9,17 @@ ResourceManager::ResourceManager()
 
 ResourceManager::~ResourceManager()
 {
-
+    //TODO delete m_geometryStorage contents
 }
 
 void ResourceManager::loadGeometry(std::string name, std::string path)
 {
-
+    //TODO implement .obj-Loader
 }
 
 std::unique_ptr<Geometry> ResourceManager::getGeometry(std::string name)
 {
-
+    if(m_geometryStorage.count(name) == 0)
+        m_geometryStorage[name] = std::shared_ptr<std::unique_ptr<Geometry>>(new std::unique_ptr<Geometry>(new Geometry()));
+    return m_geometryStorage[name];
 }
