791cdc0a2ba7a108492f0f675a5c8251cf80b92e
Fabian Dumke <fabian.dumke@student.hpi.de>
2014-12-11 13:04:47 +0100

    Implements soundmanager

--- source/resources/soundmanager.cpp
+++ source/resources/soundmanager.cpp
@@ -1,1 +1,64 @@
+#include <QSound>
 
+#include "soundmanager.h"
+
+#include <QDebug> //TODO remove
+
+//TODO use unique or shared pointer instead of standard pointer for QSound
+
+namespace terminus
+{
+
+SoundManager * SoundManager::m_instance = nullptr;
+
+SoundManager * SoundManager::getInstance()
+{
+    if(m_instance == nullptr)
+    {
+        m_instance = new SoundManager();
+    }
+
+    return m_instance;
+}
+
+SoundManager::SoundManager()
+{
+}
+
+void SoundManager::initialize()
+{
+    QSound * soundOne = new QSound("sounds/test.wav");
+    m_sounds["test"] = soundOne;
+}
+
+SoundManager::~SoundManager()
+{
+    //TODO delete m_sounds contents
+    std::map<QString, QSound *>::iterator it = sounds().begin();    //use const auto instead
+
+    qDebug() << "am i called?";
+
+    for(it; it != sounds().end(); it++)
+    {
+        delete it->second;                                               //is second correct?
+    }
+}
+
+void SoundManager::playSound(QString name)
+{
+    qDebug() << "right before playing";
+    sound(name)->play();
+    //QSound::play(name);
+}
+
+QSound * SoundManager::sound(QString name)
+{
+    return m_sounds[name];
+}
+
+std::map<QString, QSound *> SoundManager::sounds()
+{
+    return m_sounds;
+}
+
+}   //terminus
--- source/resources/soundmanager.h
+++ source/resources/soundmanager.h
@@ -1,4 +1,34 @@
-#ifndef SOUNDMANAGER_H
-#define SOUNDMANAGER_H
+#pragma once
 
-#endif // SOUNDMANAGER_H
+#include <QSound>
+
+namespace terminus
+{
+
+class SoundManager
+{
+
+public:
+    static SoundManager * getInstance();
+protected:
+    static SoundManager * m_instance;
+
+public:
+    virtual ~SoundManager();
+protected:
+    SoundManager();
+    void initialize();
+
+public:
+    void playSound(QString name);
+    QSound * sound(QString name);
+    std::map<QString, QSound *> sounds();
+
+protected:
+    std::map<QString, QSound *> m_sounds;
+
+
+
+};
+
+}  //terminus
