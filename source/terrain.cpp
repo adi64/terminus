9787b20786a909a7c0ed7ebb0a97aa0226834902
Florian Richter <florian.richter@student.hpi.uni-potsdam.de>
2015-01-16 14:56:33 +0100

    change GL_RGBA16F to GL_RGBA16F_EXT

--- source/terrain.cpp
+++ source/terrain.cpp
@@ -111,7 +111,7 @@ void Terrain::allocateTerrainMap(QOpenGLFunctions & gl) const
 
     gl.glGenTextures(1, &m_terrainMap);
     gl.glBindTexture(GL_TEXTURE_2D, m_terrainMap);
-    gl.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, m_level.totalVertexCountS(), m_level.totalVertexCountT(), 0, GL_RGBA, GL_FLOAT, data);
+    gl.glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F_EXT, m_level.totalVertexCountS(), m_level.totalVertexCountT(), 0, GL_RGBA, GL_FLOAT, data);
     gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
