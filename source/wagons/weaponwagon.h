c8ba9fa046a4279c3c32af1a206801251a7fde9a
Adrian Holfter <aholfter+github@googlemail.com>
2015-02-02 14:38:33 +0100

    add missing newline in detailed description

--- source/wagons/weaponwagon.h
+++ source/wagons/weaponwagon.h
@@ -24,6 +24,7 @@ protected:
     /*!
      * \brief Spawn a projectile and apply force
      * \param force Force vector in global coordinates (will not be rotated to match object's rotation)
+     *
      * Schedules a projectile spawn (for next frame) with some position offset, copies the current velocity to the projectile and applies the given force to it.
      */
     void fire(QVector3D force);
