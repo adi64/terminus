a5d48be78e9cae886a973f08c05738633a10a6e2
Adrian Holfter <aholfter+github@googlemail.com>
2015-02-02 14:25:22 +0100

    add a bit of doxygen-style documentation

--- source/dynamicphysicsobject.h
+++ source/dynamicphysicsobject.h
@@ -12,7 +12,18 @@ public:
 
     virtual void update(int elapsedMilliseconds) override;
 
+    /*!
+     * \brief Apply central force
+     * \param force
+     * This is just a wrapper around btRigidBody::applyCentralForce(const btVector3 &force)
+     */
     virtual void applyForce(const QVector3D & force);
+
+    /*!
+     * \brief Set the object's velocity, discarding any previously applied forces
+     * \param velocity
+     * This is just a wrapper around btRigidBody::setLinearVelocity(const btVector3 &velocity)
+     */
     virtual void setLinearVelocity(const QVector3D & velocity);
 };
 
